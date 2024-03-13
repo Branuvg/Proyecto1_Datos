f419ca01777e6ef984429319312d3c864abc92cf
DijanU <156262584+DijanU@users.noreply.github.com>
2024-03-13 08:47:14 -0600

    SetQ y QUOTE
    SetQ y Quote

---
+++ Variable.java
@@ -0,0 +1,19 @@
+public class Variable {
+
+    private String name;
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+    public void setValue(int value) {
+        this.value = value;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
