de68c1b2a73c9da1ed2d53c6d99f9eacccca7bdd
Gabriel Bran <bra23590@uvg.edu.gt>
2024-03-20 08:16:58 -0600

    prefix calc

--- PrefixCalc.java
+++ PrefixCalc.java
@@ -1,41 +1,46 @@
+import java.util.Arrays;
+
 public class PrefixCalc {
 
+    private static String[] operators = {"+","-","*","/"};
+
     public static double calcularPrefijo(String expresion) {
-        String[] tokens = expresion.split(" ");
-
+        String[] val = expresion.split(" ");
         Stack<Double> pila = new Stack<>();  // Usamos la clase Stack proporcionada
+        boolean flag = true;
+        Double num1;
+        Double result = (double) 0;
+        Double num2;
 
-        for (int i = tokens.length - 1; i >= 0; i--) {
-            String token = tokens[i];
-            if (esOperador(token)) {
-                double num2 = pila.pop();
-                double num1 = pila.pop();
-
-                switch (token) {
-                    case "+":
-                        pila.push(num1 + num2);
-                        break;
-                    case "-":
-                        pila.push(num1 - num2);
-                        break;
-                    case "*":
-                        pila.push(num1 * num2);
-                        break;
-                    case "/":
-                        pila.push(num1 / num2);
-                        break;
+        for (int i = val.length - 1; i >= 0; i--) {
+            if(Arrays.asList(operators).contains(val[i])){
+                if (pila.count() >= 2){
+                    num1 = pila.pop();
+                    num2 = pila.pop();
+                    switch (val[i]) {
+                        case "+":
+                            pila.push(num1 + num2);
+                            break;
+                        case "-":
+                            pila.push(num1 - num2);
+                            break;
+                        case "*":
+                            pila.push(num1 * num2);
+                            break;
+                        case "/":
+                            pila.push(num1 / num2);
+                            break;
+                    }
+                }
+                else{ //Faltan numeros
+                    System.out.println("Error: Le faltaron operandos");
+                    flag = false;
                 }
             } else {
-                pila.push(Double.parseDouble(token));
+                pila.push(Double.parseDouble(val[i]));
             }
         }
-
-        return pila.top();
-    }
-
-    private static boolean esOperador(String token) {
-        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
+        if (flag) return result;
+        else return (Double) null;
     }
 }
-
-
