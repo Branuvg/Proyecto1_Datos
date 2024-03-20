a33fb4a49def95e833806b2b3c8f61bd16753d5f
DijanU <156262584+DijanU@users.noreply.github.com>
2024-03-20 08:06:19 -0600

    quote
    not debugged

--- Operator.java
+++ Operator.java
@@ -1,5 +1,37 @@
+import java.util.ArrayList;
+
 public class Operator {
 
     //esta clase debe ser intanciada como SmoothOperator
 
+    public void setQ(ArrayList<String> lisp){
+        
+    }
+
+    public String quote(String func){
+        String value = "";
+        String[] tokens = func.split("");
+        int control=0;
+        for(int i = 0; i < tokens.length-1; i++){
+            if(tokens[i].equals("quote")||tokens[i].equals("QUOTE")||tokens[i].equals("'")){
+                i++;
+                control = i;
+            }
+        }
+        for(int n = control; n< tokens.length-1;n++ ){
+            if(n == tokens.length-2){
+                value = value + tokens[n];
+            }
+            else {
+                value = value + tokens[n]+" ";
+            }
+        }
+        return value;
+    }
+
+    public String cond(){
+        String cond = "";
+
+        return cond;
+    }
 }
